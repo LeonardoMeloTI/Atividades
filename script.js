c6c1fdd111e70251688c1373b605f47ddac214a4
LeonardoMeloTI <leo_melo96@hotmail.com>
2022-06-19 19:14:19 -0300

    Atividade 2º Entregue

--- script.js
+++ script.js
@@ -1,22 +1,63 @@
-// 1º Atividade entregue
+// -------------------------------->  1º Atividade   <--------------------------------
 
-let nome = "leonardo";
-console.log(nome)
+// let nome = "leonardo";
+// console.log(nome)
 
-let numero = 26;
-console.log(numero)
+// let numero = 26;
+// console.log(numero)
 
-let ano = 2022 - numero;
-console.log(ano)
+// let ano = 2022 - numero;
+// console.log(ano)
 
-let resultado = ano + numero ;
-console.log(resultado)
+// let resultado = ano + numero ;
+// console.log(resultado)
 
-console.log("Estou mexendo no console.log")
+// console.log("Estou mexendo no console.log")
 
-let quartaFeira = true;
-console.log(quartaFeira)
+// let quartaFeira = true;
+// console.log(quartaFeira)
 
-console.log(typeof quartaFeira)
+// console.log(typeof quartaFeira)
 
 
+// -------------------------------->  2º Atividade   <--------------------------------
+
+let idade = 18;
+
+if(idade >= 18){
+    console.log("Você é maior de idade");
+}else{
+    console.log("você é menor de idade,infelizmente")
+}
+
+let human = false;
+
+if(idade >= 18 && human === true){
+    console.log("Você é humano");
+}else{
+    console.log("você é de outro planeta")
+}
+
+let aniversario = "fevereiro";
+
+if(aniversario === "janeiro" || aniversario === "dezembro"){
+    console.log("Parabéns para você");
+}else{
+    console.log("Só ano que vem seu aniversário");
+}
+
+let nome = "Rosana";
+
+if(nome[0] === "R"){
+    console.log("O seu nome é bonito")
+}else{
+    console.log("Seu nome não é tão incomum")
+}
+
+let sobrenome = "Amorin";
+
+if(sobrenome[0] === "E" || sobrenome.length >= 6){
+    console.log("Seu sobrenome é bonito")
+}else{
+    console.log("O seu sobrenome é comum")
+}
