822321f4b251c5c56179ce1509b366be3b4b5c49
LeonardoMeloTI <leo_melo96@hotmail.com>
2022-06-19 15:40:12 -0300

    Atividade01 entregue

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+// 1º Atividade entregue
+
 let nome = "leonardo";
+console.log(nome)
 
-let numero = 11;
+let numero = 26;
+console.log(numero)
 
-let 
+let ano = 2022 - numero;
+console.log(ano)
+
+let resultado = ano + numero ;
+console.log(resultado)
+
+console.log("Estou mexendo no console.log")
+
+let quartaFeira = true;
+console.log(quartaFeira)
+
+console.log(typeof quartaFeira)
+
+
